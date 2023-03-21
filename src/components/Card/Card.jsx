1066d82cd044dc7d61ec9d0bc75af8bf6172d1d6
Anna Atiskova <anna.atiskova@gmail.com>
2023-03-22 01:07:30 +0200

    card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,72 @@
-import { CardStyled, Section } from "./Card.styled";
+import {
+  ActiveButton,
+  Avatar,
+  AvatarBox,
+  Button,
+  CardStyled,
+  Chat,
+  List,
+  Logo,
+  Section,
+  Text,
+} from './Card.styled';
+import logo from '../../image/logo.png';
+import chat from '../../image/chat.png';
+import line from '../../image/line.png';
+import avatar from '../../image/avatar.png';
+import { useState } from "react";
+import { useEffect } from 'react';
 
 export const Card = () => {
+  const [follow, setFollow] = useState(JSON.parse(localStorage.getItem('follow')))
+  const [followers, setFollowers] = useState(!follow ? (100500) : (JSON.parse(localStorage.getItem('followers'))));
+
+  const onFollow = () => {
+      setFollowers(prevState => prevState + 1);
+      setFollow(true)
+    };
+
+    const onUnFollow = () => {
+      setFollowers(prevState => prevState - 1);
+      setFollow(false)
+    };
+
+  useEffect(() => {
+    localStorage.setItem('followers', JSON.stringify(followers));
+    localStorage.setItem('follow', JSON.stringify(follow));
+  }, [followers, follow])
+
+
   return (
     <Section>
       <CardStyled>
-          <div>
-            <svg>
-              <use href="../../image/logo.svg"></use>
-            </svg>
-            <img src="../../image/chat.png" alt="chat" />
-          </div>
-          <div>
-            <img src="../../image/line.png" alt="line" />
-            <img src="../../image/avatar.png" alt="avatar" />
-          </div>
+        <div style={{ height: 214 }}>
+          <Logo src={logo} alt="logo" />
+          <Chat src={chat} alt="chat" />
+        </div>
+        <AvatarBox>
+          <img src={line} alt="line" />
+          <Avatar src={avatar} alt="avatar" />
+        </AvatarBox>
+        <div>
+          <List>
+            <li>
+              <Text>777 tweets</Text>
+            </li>
+            <li>
+              <Text>{followers.toLocaleString('ja-JP')} Followers</Text>
+            </li>
+          </List>
+          {follow ? (
+            <ActiveButton type="button" onClick={onUnFollow}>
+              Following
+            </ActiveButton>
+          ) : (
+            <Button type="button" onClick={onFollow}>
+              Follow
+            </Button>
+          )}
+        </div>
       </CardStyled>
     </Section>
   );
--- src/components/Card/Card.styled.js
+++ src/components/Card/Card.styled.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Section = styled.section`
-padding: 40px;
-`
+  padding: 40px;
+`;
 
 export const CardStyled = styled.div`
   background: linear-gradient(
@@ -15,3 +15,78 @@ export const CardStyled = styled.div`
   height: 460px;
   border-radius: 20px;
 `;
+
+export const Logo = styled.img`
+  padding-top: 20px;
+  padding-left: 20px;
+`;
+
+export const Chat = styled.img`
+  margin-top: -16px;
+  padding-left: 36px;
+  margin-bottom: 14px;
+`;
+
+export const AvatarBox = styled.div`
+  position: relative;
+  width: 380px;
+  line-height: 0;
+`;
+
+export const Avatar = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 80px;
+  height: 80px;
+`;
+
+export const List = styled.ul`
+list-style: none;
+margin: 0;
+margin-top: 55px;
+padding-left: 0;
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 16px;
+`
+
+export const Text = styled.p`
+margin: 0;
+font-family: Montserrat, sans-serif;
+text-transform: uppercase;
+font-size: 20px;
+line-height: calc(24 / 20);
+font-weight: 500;
+color: #EBD8FF;
+`
+
+export const Button = styled.button`
+font-family: Montserrat, sans-serif;
+text-transform: uppercase;
+display: block;
+padding: 14px 56px;
+margin: 26px auto;
+border-radius: 10px;
+border: transparent;
+font-weight: 600;
+font-size: 18px;
+line-height: calc(22 / 18);
+background-color: #EBD8FF;
+`
+
+export const ActiveButton = styled.button`
+font-family: Montserrat, sans-serif;
+text-transform: uppercase;
+display: block;
+padding: 14px 39px;
+margin: 26px auto;
+border-radius: 10px;
+border: transparent;
+font-weight: 600;
+font-size: 18px;
+line-height: calc(22 / 18);
+background-color: #5CD3A8;
+`

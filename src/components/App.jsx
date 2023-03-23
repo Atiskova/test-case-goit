5dbd3c642ed2fb2d5275df114aaeb5e1594dd091
Anna Atiskova <anna.atiskova@gmail.com>
2023-03-24 01:27:58 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,26 @@
-import { Card } from "./Card/Card";
+// import { Card } from './Card/Card';
+import plate1 from '../image/3623760.png';
+import plate2 from '../image/2205.png'
+import plate7 from '../image/2206.png'
+import plate3 from '../image/3637115.png';
+import plate4 from '../image/3674377.png';
+import plate5 from '../image/3702865.png';
+import plate6 from '../image/5451609.png';
+
 
 export const App = () => {
   return (
-   <Card />
+    <>
+      {/* <Card /> */}
+      <div>
+        <img src={plate1} alt="plate" width="1200"/>
+        <img src={plate2} alt="plate" width="1200"/>
+        <img src={plate7} alt="plate" width="1200"/>
+        <img src={plate3} alt="plate" width="1200"/>
+        <img src={plate4} alt="plate" width="1200"/>
+        <img src={plate5} alt="plate" width="1200"/>
+        <img src={plate6} alt="plate" width="1200"/>
+      </div>
+    </>
   );
 };
